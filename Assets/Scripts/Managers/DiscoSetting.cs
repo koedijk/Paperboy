d0f400af9ce1519dbe3a517e06206d35b5efe720
koedijk <17798@ma-web.nl>
2016-03-17 19:46:46 +0100

    Paperboy Original

---
+++ Assets/Scripts/Managers/DiscoSetting.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiscoSetting : MonoBehaviour
+{
+	private static bool spawned = false;
+
+	public AudioClip DiscoClip;
+
+	public bool IsDisco = false; 
+
+	void Awake()
+	{
+		if(DiscoClip == null)
+			DiscoClip = (AudioClip)Resources.Load("Disco");
+
+		if(spawned == false)
+		{
+			spawned = true;
+			
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			DestroyImmediate(gameObject);
+		}
+	}
+
+	void Start()
+	{
+		if(Application.loadedLevelName == "PlayScene")
+		{
+			Global.Instance.IsDisco = IsDisco;
+			Camera.main.gameObject.audio.clip = DiscoClip;
+		}
+	}
+}
